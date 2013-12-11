6df78b1109a1583fea3c87444b5f86b6af84efbf
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2013-12-11 17:32:53 +0200

    Added catch header file

---
+++ src/entry.cpp
@@ -0,0 +1,6 @@
+/**
+ * This file will include all test case headers.
+ * Eventually includes will be removed and main function will be written here.
+ */
+#define CATCH_CONFIG_RUNNER
+#include "tests/asm_tests.hpp"
