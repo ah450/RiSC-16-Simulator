829cd7dee7823e94a3687b2f74959bdf33d097ff
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2013-12-13 13:01:33 +0200

    Created  Logger interface and BasicLogger class;

---
+++ include/logger.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+struct ILogger {
+    virtual ILogger& operator<<(const std::string &) = 0;
+    virtual ~ILogger(){};
+};
+
+
+struct BasicLogger : public ILogger {
+    ILogger& operator<<(const std::string &str) override {std::cerr << str << '\n'; return *this;}
+};
