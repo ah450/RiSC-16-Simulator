fcd739a81ba58e27b73dd19c8ac40d5b910e51f9
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2013-12-17 07:48:28 +0200

    Modifications to Sym

--- include/asm/internal/internal.hpp
+++ include/asm/internal/internal.hpp
@@ -21,6 +21,7 @@ typedef std::uint16_t addr_t;
 typedef std::uint16_t pc_t;
 typedef std::pair<std::unique_ptr<bfs::ifstream>, bfs::path> file_path_pair_t;
 typedef std::vector<file_path_pair_t> ifvector_t;
+
 enum class SymType {PTR, DATA};
 enum class InstType {LW, SW, ADDI, JMP, LI, JALR, JALI, ADD, SUB, MUL,
                     DIV, HALT, AND, OR, XOR, NOT, RET, BEQ, BNE, BGT, BGE, BLT, BLE};
