67496d8079450cb3c7ebbf8dbf55c73d88b0a0f8
Kareem <lkareem.alil@gmail.com>
2013-12-22 15:10:48 +0200

    reservation stations header file

--- include/tomasulo/reservation_station.hpp
+++ include/tomasulo/reservation_station.hpp
@@ -1,27 +1,40 @@
 #ifndef RESERVATION_STATION_H
 #define RESERVATION_STATION_H
 #include <stdint.h>
+#include <bitset>
+#include "instruction.hpp"
 
-enum class OP{MUL, DIV, ADD, SUB, BRANCH, JUMP, LOAD, STORE};
+enum class FUNCTION_UNIT{
+    ADD, MULL, LOAD, BRANCH, LOGIC //LI is in ADD FU.
+};
+
+enum class STATE {ISSUED, EXECUTED, WRITTEN, IDLE};
 
 class reservation_station{
 
 private:
-	bool busy;
-	OP op;
-	uint16_t vj;
-	uint16_t vk;
-	uint16_t qj;
-	uint16_t qk;
-	unsigned int destination;
-	uint16_t address;
-	unsigned int delay_cycles;
+    const unsigned int id;
+    bool busy;
+    STATE state;
+    FUNCTION_UNIT fu;
+    uint16_t vj;
+    uint16_t vk;
+    unsigned int qj;
+    unsigned int qk;
+    unsigned int destination;
+    uint16_t address;
+    unsigned int delay_cycles;
 
 public:
-	reservation_station(OP operation_type,  uint16_t vj, 
-		uint16_t vk, uint16_t qj, uint16_t qk, 
-		unsigned int delay, unsigned int address);
-	virtual uint16_t execute(uint16_t operand1, uint16_t operand2) = 0;
+    reservation_station(FUNCTION_UNIT fu,
+        unsigned int delay);
+    bool is_busy(){return busy;}
+    bool operator==(reservation_station & rs){return id== rs.id;}
+    auto get_id(){return id;}
+    void flush();
+    STATE tick(tomasulo & t);
+    STATE get_state(){return state;}
+    bool issue(Instruction inst);
 };
 
 #endif
--- src/core/tomasulo/reservation_station.cpp
+++ src/core/tomasulo/reservation_station.cpp
@@ -1,15 +1,9 @@
 #include "../../include/tomasulo/reservation_station.hpp"
 
-reservation_station::reservation_station(OP operation_type,  uint16_t vj, 
-	uint16_t vk, uint16_t qj, uint16_t qk, 
-	unsigned int delay, unsigned int address){
-	busy = true;
-	this->vj = vj;
-	this->vk = vk;
-	this->qj = qj;
-	this->qk = qk;
-	op = operation_type;
+reservation_station::reservation_station(OP operation_type, unsigned int delay){
+    static unsigned int counter = 0;
+    id = counter ++;
+    busy = false;
+    op = operation_type;
 	delay_cycles = delay;
-	this->address = address;
-
 }
