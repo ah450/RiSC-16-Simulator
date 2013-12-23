22014634d65b6f72f2b6ade85d01b28699be30ba
muhammed <muhammedabuali@gmail.com>
2013-12-23 13:04:30 +0200

    headers for instruction buffer #15

--- include/tomasulo/inst_buffer.hpp
+++ include/tomasulo/inst_buffer.hpp
@@ -1,20 +1,18 @@
 #ifndef INST_BUFFER_H
 #define INST_BUFFER_H
 #include "inst_buffer_row.hpp"
-#include <queue>
+#include <list>
 #include <stdint.h>
 
 class inst_buffer{	
 private:
 	unsigned int number_of_entries;
-    std::queue <inst_buffer_row> entries;
+    std::list <inst_buffer_row> entries;
 
 public:
     inst_buffer(unsigned int size);
-	virtual bool insert_instruction(uint16_t instruction);
-	virtual unsigned int size();
-
-    //add a function to change the register board when issue
+    set_i_cache(std::shared_ptr<i_memory> instruction_cache);
+    virtual void tick (tomasulo &t);//handles fetching and issuing
 };
 
 #endif
