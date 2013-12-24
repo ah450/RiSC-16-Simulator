2372b009832202fbd18fb9104f0ff2dac7985b26
Kareem <lkareem.alil@gmail.com>
2013-12-24 18:12:20 +0200

    #16 sign extend for immediate values and adding function to compute predicted address of branch

--- include/tomasulo/instruction.hpp
+++ include/tomasulo/instruction.hpp
@@ -2,7 +2,7 @@
 #define INSTRUCTION_H
 #include <stdexcept>
 
-
+// 23 operations for 23 different types of instructions
 enum class OP {LW, SW, ADDI, JMP, LI, JALR, JALI, ADD, SUB,
     MUL, DIV, HALT, AND, OR, XOR, NOT, RET, BEQ, BNE, BGT,
     BLT, BGE, BLE};
@@ -19,6 +19,7 @@ struct Instruction{
     OP get_type();
     std::uint8_t get_operand(const uint8_t index);
     std::uint16_t get_immediate(); //implement exceptions for non immediate instructions
+    std::uint16_t get_branch_addr(tomasulo & t);//returns the predicted address
 };
 
 
--- src/core/tomasulo/instruction.cpp
+++ src/core/tomasulo/instruction.cpp
@@ -7,77 +7,77 @@ OP Instruction::get_type(){
     if(offset3 != 7){
         switch(offset3){
             case 0:
-                op = LW;
+                op = OP::LW;
                 break;
             case 1:
-                op = SW;
+                op = OP::SW;
                 break;
             case 2:
-                op = ANDI;
+                op = OP::ANDI;
                 break;
             case 3:
-                op = JMP;
+                op = OP::JMP;
                 break;
             case 4:
-                op = LI;
+                op = OP::LI;
                 break;
             case 5:
-                op = JALR;
+                op = OP::JALR;
                 break;
             case 6:
-                op = JALI;
+                op = OP::JALI;
                 break;
         }
     }
     else{
         switch(offset4){
             case 0:
-                op = ADD;
+                op = OP::ADD;
                 break;
             case 1:
-                op = SUB;
+                op = OP::SUB;
                 break;
             case 2:
-                op = MUL;
+                op = OP::MUL;
                 break;
             case 3:
-                op = DIV;
+                op = OP::DIV;
                 break;
             case 4:
-                op = HALT;
+                op = OP::HALT;
                 break;
             case 5:
-                op = AND;
+                op = OP::AND;
                 break;
             case 6:
-                op = OR;
+                op = OP::OR;
                 break;
             case 7:
-                op = XOR;
+                op = OP::XOR;
                 break;
             case 8:
-                op = NOT;
+                op = OP::NOT;
                 break;
             case 9:
-                op = RET;
+                op = OP::RET;
                 break;
             case 10:
-                op = BEQ;
+                op = OP::BEQ;
                 break;
             case 11:
-                op = BNE;
+                op = OP::BNE;
                 break;
             case 12:
-                op = BGT;
+                op = OP::BGT;
                 break;
             case 13:
-                op = BLT;
+                op = OP::BLT;
                 break;
             case 14:
-                op = BGE;
+                op = OP::BGE;
                 break;
             case 15:
-                op = BLE;
+                op = OP::BLE;
                 break;
         }
     }
@@ -85,11 +85,11 @@ OP Instruction::get_type(){
 }
 
 std::uint8_t Instruction::get_operand(const uint8_t index){
-    if(index > 3){
+    if(index > 3 || index < 1){
         InstructionException("Invalid operand index is requested");
     }
     uint16_t offset4 = (binary >> 9) & 15;
-    uint16_t offset3= (binary >> 13) & 7;
+    uint16_t offset3 = (binary >> 13) & 7;
     uint8_t operand1 = (binary >> 10) & 7;
     uint8_t operand2 = (binary >> 7) & 7;
     uint8_t operand3;
@@ -98,16 +98,16 @@ std::uint8_t Instruction::get_operand(const uint8_t index){
             case 0:
             case 1:
             case 2:
+            case 5:
                 if(index > 2){
-                    InstructionException("LW, SW and ANDI instructions have only two operand");
+                    InstructionException("LW, SW and ANDI, JALR instructions have only two operands");
                 }
                 break;
             case 3:
             case 4:
-            case 5:
             case 6:
                 if(index > 1){
-                    InstructionException("JMP, LI, JALR and JALI instructions have only one operand");
+                    InstructionException("JMP, LI and JALI instructions have only one operand");
                 }
                 break;
         }
@@ -140,13 +140,21 @@ std::uint8_t Instruction::get_operand(const uint8_t index){
 std::uint16_t Instruction::get_immediate(){
     uint16_t offset3= (binary >> 13) & 7;
     uint16_t result;
-    if(offset3 != 7){
+    if(offset3 != 7 && offset3 != 5){//offset3 !=3 and not a JALR instruction
         result = binary & 127;
         if(offset3 == 3 || offset3 == 4 || offset3 == 6){
             result = binary & 1023;
+            //sign extend
+            if(result & 512 != 0){
+                result |= 0xFC00;
+            }
         }
         else{
             result = binary & 127;
+            //sign extend
+            if(result & 64 != 0){
+                result |= 0xFF80;
+            }
         }
     }
     else{
@@ -155,5 +163,69 @@ std::uint16_t Instruction::get_immediate(){
     return result;
 }
 
+enum class OP {LW, SW, ADDI, JMP, LI, JALR, JALI, ADD, SUB,
+    MUL, DIV, HALT, AND, OR, XOR, NOT, RET, BEQ, BNE, BGT,
+    BLT, BGE, BLE};
 
 
+std::uint16_t Instruction::get_branch_addr(tomasulo & t){
+    OP t = get_type();
+    if(t == OP::JMP || t == OP::JALR || t == OP::JALI || 
+        t == OP::RET || t == OP::BEQ || t == OP::BNE || 
+        t == OP::BGT || t == OP::BLT || t == OP::BGE || 
+        t == OP::BLE ){
+    }
+    std::uint16_t pc = t.PC;
+    std::uint16_t imm;
+    std::uint8_t reg;
+    std::uint16_t reg_content;
+    std::uint16_t predicted_addr;
+    switch(t){
+        case OP::JMP:
+            imm = get_immediate();
+            reg = get_operand(1);
+            if(reg == 0){
+                InstructionException("REGS[0] cannot be accessed");        
+            }
+            reg_content = t.regFile.regs[reg];
+            predicted_addr = pc + reg_content + imm + 1;
+            break;
+        case JALI:
+            imm = get_immediate();
+            predicted_addr = pc + imm + 1;
+            break;
+        case JALR:
+            reg = get_operand(2);
+            if(reg == 0){
+                InstructionException("REGS[0] cannot be accessed");        
+            }
+            reg_content = t.regFile.regs[reg];
+            predicted_addr = reg_content + pc + 1;
+            break;
+        case RET:
+            reg = get_operand(1);
+            if(reg == 0){
+                InstructionException("REGS[0] cannot be accessed");        
+            }
+            reg_content = t.regFile.regs[reg];
+            predicted_addr = reg_content;
+            break;
+        case OP::BEQ:
+        case OP::BNE:
+        case OP::BGT:
+        case OP::BLT:
+        case OP::BGE:
+        case OP::BLE:
+            reg = get_operand(3);
+            if(reg == 0){
+                InstructionException("REGS[0] cannot be accessed");        
+            }
+            reg_content = t.regFile.regs[reg];
+            predicted_addr = reg_content + pc + 1;
+            break;
+        default:
+            InstructionException("Error while computing branch address");
+            break;
+    }
+    return predicted_addr;
+}
