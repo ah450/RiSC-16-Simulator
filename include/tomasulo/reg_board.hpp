8fd4f5b87b727803d468e9126906e3d4e7f2a4d4
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2013-12-24 14:12:27 +0200

    reg board

--- include/tomasulo/reg_board.hpp
+++ include/tomasulo/reg_board.hpp
@@ -1,17 +1,38 @@
 #ifndef REG_BOARD_H
 #define REG_BOARD_H
 #include <array>
+#include <bitset>
 
 class reg_board{
 private:
 	std::array<unsigned int,7> reg_board_array;
+    std::bitset<7> used;
+    std::array<unsigned short, 7> producer;
 
 public:
 	reg_board();
-	virtual bool used(unsigned int index);
-	virtual void record(unsigned int index, unsigned int rob_index);
-	virtual void unrecord(unsigned int rob_index);
-	virtual unsigned int retrieve(unsigned int index);
+	bool used(unsigned int reg) {
+        return used.test(reg);
+    }
+	bool record(unsigned int reg, unsigned short rob_index){
+        if(!used.test(reg)) {
+            used.set(reg);
+            producer[reg] = rob_index;
+            return true;
+        }else {
+            return false;
+        }
+    }
+	void unrecord(unsigned short rob_index) {
+        for(std::size_t i = 0; i < producer.sizse(); i++) {
+            if((producer[i] == rob_index) && used.test(i)) {
+                used.reset(i);                
+            }
+        }
+    }
+	unsigned int retrieve(unsigned int reg){
+        return producer[reg];
+    }
 };
 
 #endif
