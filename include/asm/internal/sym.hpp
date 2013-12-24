848811cf9abef10d78b8f974d9806bca999fb496
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2013-12-24 13:10:07 +0200

    Header for ass::internal::Sym

--- include/asm/internal/sym.hpp
+++ include/asm/internal/sym.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "asm/internal/typedefs.hpp"
 #include <boost/filesystem.hpp>
+#include <stdexcept>
 #include <string>
 
 namespace ass { namespace internal {
@@ -8,26 +9,38 @@ namespace ass { namespace internal {
 namespace bfs = boost::filesystem;
 enum class SymType {PTR, DATA};
 
+struct SymException : std::runtime_error {
+    SymException(const std::string &what): std::runtime_error(what){}
+};
+
 class Sym : std::enable_shared_from_this<Sym>{
     bool defined_, global_;
     addr_t address_;
     SymType type_;
     const std::string name_;
     bfs::path file_;
-    std::size_t lineNum_;
+    static const bfs::path GLOBAL_FILE("GLOBAL");
 public:
     /**
      * @brief Creates an undefined label i.e forward declaration.
      */
-    Sym(cosnt std::string & name, bool global) : name_(name), defined_(false), global_(global) {}
+    Sym(cosnt std::string & name, bool global);
 
 
     /**
-     * @brief Constructs a defined Symbol.
+     * @brief Constructs a defined Symbol non global.
      */
-    Sym(bool global, const std::string &name,
-        bfs::path file, std::size_t lineNum,  addr_t value, SymType type);
+    Sym(const std::string &name,
+        const bfs::path & file, addr_t value, SymType type);
+    /**
+     * @brief Constructs a defined global Symbol.
+     */
+    Sym(const std::string &name, addr_t value, SymType type);
 
+    Sym(const Sym &) = delete;
+    Sym(Sym &&) = delete;
+    Sym & operator=(const Sym&) = delete;
+    Sym & operator=(Sym &&) = delete;
 
     bool operator==(const Sym &other) const {
         return (file_ == other.file_) && (name_ == other.name_);
@@ -35,14 +48,21 @@ public:
     bool operator<(const Sym & other) const {
         return (file_.generic_string() +  name_ ) < (other.file_.generic_string() + other.name_);
     }
-    bool defined() { return defined_;}
-    const std::string & name() {return name_;}
-    bool global() {return global_;}
-    void define() {defined_ = true;}
-    bfs::path & path(){return file_;}
-    std::size_t & lineNumber(){return lineNum_;}
-    addr_t & value(){return value_;}
 
+    bool defined() const { return defined_;}
+    const std::string & name() const {return name_;}
+    bool global() const {return global_;}
+    const bfs::path & path() const {return file_;}
+    const addr_t & value(){return value_;}
+    /**
+     * @throws SymException if already defined
+     * @throws SymException if it is a global sym
+     */
+    void define(const bfs::path & file, addr_t value, SymType type );
+    /**
+     * @throws  SymException if already defined
+     */
+    void define(addr_t value, SymType type );
 };
 
 
